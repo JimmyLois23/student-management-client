384bf85a651f6d0ec4691f5b523f864d61c7d324
Jimmy <137945889+JimmyLois23@users.noreply.github.com>
2025-03-07 01:09:02 +0530

    Update TableOne.tsx

--- src/components/Tables/TableOne.tsx
+++ src/components/Tables/TableOne.tsx
@@ -132,7 +132,7 @@ const TableOne: React.FC = () => {
   return (
     <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
-        {currentDate} Attendance Lists
+        {currentDate} Attendance Lists \\ (rafikul.career@gmail.com | 9064281142)
       </h4>
 
       <Toaster reverseOrder={false} position="top-right" />
